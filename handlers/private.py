200ed00983a3ec3ebac00181f2f02c7eaf9dc962
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-01 01:07:57 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -5,8 +5,7 @@ from config import BOT_NAME as bn
 from helpers.filters import other_filters2
 
 
-     disable_web_page_preview=True
-    
+
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
